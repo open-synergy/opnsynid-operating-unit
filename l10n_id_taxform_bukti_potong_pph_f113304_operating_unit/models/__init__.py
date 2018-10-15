d70efcac7299820e5199b10a8cd6b44f415a717c
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 04:57:15 +0700

    [8.0.1.0.0][l10n_id_taxform_bukti_potong_pph_f113304_operating_unit]

---
+++ l10n_id_taxform_bukti_potong_pph_f113304_operating_unit/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    bukti_potong_pph_f113304_in,
+    bukti_potong_pph_f113304_out)
