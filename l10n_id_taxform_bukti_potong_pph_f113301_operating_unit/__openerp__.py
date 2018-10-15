b28cf25b0d500af2801f93e38155aace1f9bc094
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 04:35:55 +0700

    [8.0.1.0.0][l10n_id_taxform_bukti_potong_pph_f113301_operating_unit]

---
+++ l10n_id_taxform_bukti_potong_pph_f113301_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Bukti Potong PPh 21 "
+            "Tidak Final (f.1.1.33.01) with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "l10n_id_taxform_bukti_potong_pph_f113301",
+        "l10n_id_taxform_bukti_potong_pph_common_operating_unit"
+    ],
+    "data": [
+        "security/bukti_potong_pph_f113301_out_security.xml"
+    ],
+    "installable": True,
+    "auto_install": True
+}
