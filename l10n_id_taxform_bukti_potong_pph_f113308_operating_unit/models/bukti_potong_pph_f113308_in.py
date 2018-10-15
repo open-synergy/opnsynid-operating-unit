93c0315003881da400d77eb3822a7a9df47df272
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-16 05:17:23 +0700

    [8.0.1.0.0][l10n_id_taxform_bukti_potong_pph_f113308_operating_unit]

---
+++ l10n_id_taxform_bukti_potong_pph_f113308_operating_unit/models/bukti_potong_pph_f113308_in.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class BuktiPotongPPhF113308In(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113308_in"
+    _inherit = [
+        "l10n_id.bukti_potong_pph_f113308_in",
+        "l10n_id.bukti_potong_pph"
+    ]
