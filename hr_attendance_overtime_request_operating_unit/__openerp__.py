216cf646382b152ddcb815d1115e2222bb4fe41f
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-22 16:54:41 +0700

    [8.0.1.0.0][hr_attendance_overtime_request_operating_unit]

---
+++ hr_attendance_overtime_request_operating_unit/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Attendance Overtime Request "
+            "with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "hr_attendance_overtime_request",
+        "operating_unit"
+    ],
+    "data": [
+        "security/hr_overtime_request_security.xml",
+        "views/hr_overtime_request_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True
+}
---
+++ hr_attendance_overtime_request_operating_unit/models/hr_overtime_request.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrOvertimeRequest(models.Model):
+    _inherit = "hr.overtime_request"
+
+    operating_unit_id = fields.Many2one(
+        string="Default Operating Unit",
+        comodel_name="operating.unit",
+        default=lambda self:
+        self.env['res.users'].
+        operating_unit_default_get(self._uid)
+    )
