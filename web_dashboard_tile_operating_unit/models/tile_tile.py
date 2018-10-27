4a34f64f97804fefdc6f7204fb22b22de81feca3
Andhitia Rama <andhitia.r@gmail.com>
2018-10-27 15:50:59 +0700

    [8.0.1.0.0] web_dashboard_operating_unit

---
+++ web_dashboard_tile_operating_unit/models/tile_tile.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class TileTile(models.Model):
+    _inherit = "tile.tile"
+
+    operating_unit_ids = fields.Many2many(
+        string="Operating Unit",
+        comodel_name="operating.unit",
+        relation="rel_tile_2_operating_unit",
+        column1="tile_id",
+        column2="operating_unit",
+    )
