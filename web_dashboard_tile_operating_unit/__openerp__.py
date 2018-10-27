bd213741918a68a413a287df7d224142c90dedc4
Andhitia Rama <andhitia.r@gmail.com>
2018-10-27 15:50:59 +0700

    [8.0.1.0.0] web_dashboard_operating_unit

---
+++ web_dashboard_tile_operating_unit/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Dashboard Tile with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Localization",
+    "depends": [
+        "web_dashboard_tile",
+        "operating_unit"
+    ],
+    "data": [
+        "security/tile_tile_security.xml",
+        "views/tile_tile_views.xml"
+    ],
+    "installable": True,
+}
