84044e60bdab360273a43997f05c04f09c01ba45
mikevhe18 <viriyananda.michael@gmail.com>
2019-02-26 13:44:31 +0700

    [8.0.1.0.0][account_asset_management_retirement_by_stolen_operating_unit]

---
+++ account_asset_management_retirement_by_stolen_operating_unit/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Asset Management Retirement by Stolen with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "depends": [
+        "account_asset_management_operating_unit",
+        "account_asset_management_retirement_by_stolen"
+    ],
+    "data": [
+        "security/account_asset_retirement_by_stolen_security.xml"
+    ],
+    "installable": True,
+    "auto_install": True
+}
