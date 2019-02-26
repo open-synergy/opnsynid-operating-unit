780b07f4ad7f7df871e598964a443de3789e2e45
mikevhe18 <viriyananda.michael@gmail.com>
2019-02-26 13:43:26 +0700

    [8.0.1.0.0][account_asset_management_impairment_operating_unit]

---
+++ account_asset_management_impairment_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Asset Management Impairment with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "depends": [
+        "account_asset_management_operating_unit",
+        "account_asset_management_impairment"
+    ],
+    "data": [
+        "security/account_asset_impairment_security.xml",
+        "security/account_asset_impairment_reversal_security.xml"
+    ],
+    "installable": True,
+    "auto_install": True
+}
