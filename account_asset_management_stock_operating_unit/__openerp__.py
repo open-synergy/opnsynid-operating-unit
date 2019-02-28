d9bced2a912dbe89bc205c67aaac9b4332ed4e0f
Andhitia Rama <andhitia.r@gmail.com>
2019-03-01 05:49:20 +0800

    [8.0.1.0.0] account_asset_management_stock_operating_unit

---
+++ account_asset_management_stock_operating_unit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Account Asset Management - Stock with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "depends": [
+        "account_asset_management_operating_unit",
+        "account_asset_management_stock",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True
+}
