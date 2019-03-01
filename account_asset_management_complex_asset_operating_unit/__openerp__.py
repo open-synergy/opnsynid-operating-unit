0a1a5ef1b0870339b3ce1c591186ac505ae667ea
mikevhe18 <viriyananda.michael@gmail.com>
2019-03-01 10:05:12 +0700

    [8.0.1.0.0][account_asset_management_complex_asset_operating_unit]

---
+++ account_asset_management_complex_asset_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Complex Fixed Assets Management with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "depends": [
+        "account_asset_management_operating_unit",
+        "account_asset_management_complex_asset"
+    ],
+    "data": [
+        "security/account_complex_asset_removal_security.xml",
+        "security/account_complex_asset_installation_security.xml"
+    ],
+    "installable": True,
+    "auto_install": True
+}
